51651829b928d40c47a188f977eb7ee1ac3164cd
Kayk Santos <80800368+KaykSantos@users.noreply.github.com>
2023-05-03 15:55:04 -0300

    Atualização - Final Aula
    Integração tela cadastro com css e php

--- cadastro/index.php
+++ cadastro/index.php
@@ -16,84 +16,104 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Cadastro</title>
-    <style>
-        body{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-        }
-        fieldset{
-            width: 400px;
-        }
-        input{
-            width: 350px;
-            padding: 2px;
-        }
-        select{
-            width: 350px;
-            padding: 2px;
-        }
-    </style>
 </head>
 <body>
-    <form action="" method="post">
-        <fieldset>
-            <legend>Cadastro</legend>
-            <input type="number" id="rm" name="rm" placeholder="Digite seu RM: "> <br><br>
-            <input type="text" id="name" name="name" placeholder="Digite seu nome: "> <br><br>
-            <input type="email" id="email" name="email" placeholder="Digite seu email: "> <br><br>
-            <input type="password" id="password" name="password" placeholder="Digite sua senha:"> <br><br>
-            <select name="curso" id="curso"> <br><br>
-                <option value="10">Escolha seu curso: </option>
-                <option value="1">1º ano - Médio de Desenvolvimento de Sistemas</option>
-                <option value="2">2º ano - Médio de Desenvolvimento de Sistemas</option>
-                <option value="3">3º ano - Médio de Desenvolvimento de Sistemas</option>
-                <option value="4">1º ano - Programação de Jogos Digitais</option>
-                <option value="5">2º ano - Programação de Jogos Digitais</option>
-                <option value="6">3º ano - Programação de Jogos Digitais</option>
-                <option value="7">1º ano - Administração</option>
-                <option value="8">2º ano - Administração</option>
-                <option value="9">3º ano - Administração</option>
-            </select>
-            <br><br>
-            <button name="submit">Cadastrar</button>  
-            <p>Já possui conta?<a href="../login"> Faça login!</a></p>
-        </fieldset>
+    <main>
+        <div id="container-register">
+            <div id="titles-container">
+                <h1>Cadastro</h1>
+                <h6>Faça seu cadastro</h6>
+            </div>
+            
+            <form method="post">
+                <div id="inputs-container">
+                    <input type="number" name="rm" class="input" placeholder="RM">
+                    <input type="text" name="name" class="input" placeholder="Nome">
+                    <input type="email" name="email" class="input" placeholder="E-mail">
+                    <div><input type="password" id="password1" class="input" placeholder="Senha" name="password"><img src="../images/eye.png" class="img-eye" id="btn-eye1"></div>
+                    <div><input type="password" id="password2" class="input" placeholder="Confirmar a senha"    ><img src="../images/eye.png" class="img-eye" id="btn-eye2"></div>
+                    <div id="select-container">
+                        <select name="curso" id="curso" name="curso">
+                        <option value="10">Escolha seu curso: </option>
+                        <option value="1">1° ano - Médio de Desenvolvimento de Sistemas </option>
+                        <option value="2">2° ano - Médio de Desenvolvimento de Sistemas </option>
+                        <option value="3">3° ano - Médio de Desenvolvimento de Sistemas </option>
+                        <option value="4">1° ano - Programação de Jogos Digitais </option>
+                        <option value="5">2° ano - Programação de Jogos Digitais </option>
+                        <option value="6">3° ano - Programação de Jogos Digitais </option>
+                        <option value="7">1° ano - Administração </option>
+                        <option value="8">2° ano - Administração </option>
+                        <option value="9">3° ano - Administração </option>
+                        </select>
+                    </div>
+                    <div id="btn-container">
+                        <button name="submit">Enviar</button>
+                    </div>
+                </div>
+            </form>
+            <div id="login-container">
+                <p>Já possui conta?</p>
+                <a href="../login">Faça Login!</a>
+            </div>
+            <?php
+                if(isset($_SESSION['cad-effect'])):
+            ?>   
+            <div id="error-cad">
+                <p>Cadastro realizado com sucesso! Agora realize seu login.</p>
+            </div>
+            <?php
+                endif;
+                unset($_SESSION['cad-effect']);
+            ?>
 
-        <?php
-            if(isset($_SESSION['cad-effect'])):
-        ?>   
-        <div id="error-cad">
-            <p>Cadastro realizado com sucesso! Agora realize seu login.</p>
-        </div>
-        <?php
-            endif;
-            unset($_SESSION['cad-effect']);
-        ?>
+            <?php
+                if(isset($_SESSION['email-error'])):
+            ?>   
+            <div id="error-email">
+                    <p>Email já cadastrado, tente utilizar outro ou faça login!</p>
+            </div>
+            <?php
+                endif;
+                unset($_SESSION['email-error']);
+            ?>
 
-        <?php
-            if(isset($_SESSION['email-error'])):
-        ?>   
-        <div id="error-email">
-                <p>Email já cadastrado, tente utilizar outro ou faça login!</p>
+            <?php
+                if(isset($_SESSION['cad-error'])):
+            ?>   
+            <div id="error-cad">
+                <p>Erro ao realizar cadasatro, tente novamente!</p>
+            </div>
+            <?php
+                endif;
+                unset($_SESSION['cad-error']);
+            ?>
         </div>
-        <?php
-            endif;
-            unset($_SESSION['email-error']);
-        ?>
+    </main>
+    <script>
 
-        <?php
-            if(isset($_SESSION['cad-error'])):
-        ?>   
-        <div id="error-cad">
-            <p>Erro ao realizar cadasatro, tente novamente!</p>
-        </div>
-        <?php
-            endif;
-            unset($_SESSION['cad-error']);
-        ?>
-    </form>
+        let btn1 = document.getElementById('btn-eye1');
+        btn1.addEventListener('click', function(){
+            let input = document.getElementById('password1');
+
+            if(input.getAttribute('type') == 'password') {
+                input.setAttribute('type', 'text');
+            }else {
+                input.setAttribute('type', 'password');
+            }
+        });
+
+        let btn2 = document.getElementById('btn-eye2');
+        btn2.addEventListener('click', function(){
+            let input = document.getElementById('password2');
+
+            if(input.getAttribute('type') == 'password') {
+                input.setAttribute('type', 'text');
+            }else {
+                input.setAttribute('type', 'password');
+            }
+        });
+    </script>
 </body>
 </html>
